0801175b45155a19597120354cd06eb0528662ef
oussama <aouidetoussama@gmail.com>
2020-08-26 14:59:47 +0200

    init commit

---
+++ src/main/java/com/kata/demoKata/DemoKataApplication.java
@@ -0,0 +1,13 @@
+package com.kata.demoKata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/demoKata/DemoKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.demoKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
